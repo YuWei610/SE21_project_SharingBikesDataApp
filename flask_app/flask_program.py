b58827c8de690956be792e27c04dba64353344ec
YuWei610 <yu-wei.lin@ucdconnect.ie>
2025-02-19 19:21:16 +0000

    flask_build_on_EC2

---
+++ flask_app/flask_program.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+
+from flask import Flask
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     # 将此处替换为你自己的 Google Maps API 密钥
+#     google_maps_api_key = "YOUR_API_KEY"
+#     return render_template('index.html', google_maps_api_key=google_maps_api_key)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/user")
+def user():
+    return app.send_static_file('user.html')
+
+@app.route("/map")
+def main():
+    return render_template("map.html")
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host="172.31.44.41", port=5000)
